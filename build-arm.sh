5e93d8bc39d802c131c28a22614a2b83bd36f098
blott <blott.comms@gmail.com>
2017-05-13 14:39:41 +0200

    use base image with all dependencies pre-built manually

--- build-arm.sh
+++
@@ -1,6 +0,0 @@
-docker run --rm --privileged multiarch/qemu-user-static:register --reset
-mkdir tmp
-pushd tmp &&
-  curl -L -o qemu-arm-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/v2.6.0/qemu-arm-static.tar.gz &&
-  tar xzf qemu-arm-static.tar.gz &&
-  popd
