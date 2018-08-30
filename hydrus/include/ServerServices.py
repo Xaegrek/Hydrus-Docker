dd29e0c784ea9ae4e6e36b0760bf892a41a727b7
xaegrek <xaegrek@gmail.com>
2018-08-30 19:15:00 -0400

    submodule back

--- hydrus/include/ServerServices.py
+++
@@ -1,5 +0,0 @@
-import HydrusConstants as HC
-import HydrusSerialisable
-
-# Service object that has bandwidth tracking and rules
-# file repos also have max storage
--- hydrus/include/TestClientData.py
+++
@@ -1,7 +0,0 @@
-import ClientConstants as CC
-import ClientImportOptions
-import ClientImportFileSeeds
-import HydrusConstants as HC
-import HydrusExceptions
-import os
-import unittest
